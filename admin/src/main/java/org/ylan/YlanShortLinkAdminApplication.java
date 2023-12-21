c78c3925c1837acf26c00d85a6a19f554a3e0c5f
ylan <pepsiwyl@gmail.com>
2023-12-21 23:48:04 +0800

    Init Project

---
+++ admin/src/main/java/org/ylan/YlanShortLinkAdminApplication.java
@@ -0,0 +1,15 @@
+package org.ylan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ylan
+ */
+
+@SpringBootApplication
+public class YlanShortLinkAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(YlanShortLinkAdminApplication.class, args);
+    }
+}
---
+++ project/src/main/java/org/ylan/YlanShortLinkProjectApplication.java
@@ -0,0 +1,15 @@
+package org.ylan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ylan
+ */
+
+@SpringBootApplication
+public class YlanShortLinkProjectApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(YlanShortLinkProjectApplication.class, args);
+    }
+}
