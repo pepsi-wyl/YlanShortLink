6519f38b34a5f6a5e589166140856b6d52aedfb7
ylan <pepsiwyl@gmail.com>
2023-12-23 14:01:34 +0800

    1.Redis 缓存常量类-用户注册分布式锁key

---
+++ admin/src/main/java/org/ylan/common/constant/RedisCacheConstant.java
@@ -0,0 +1,14 @@
+package org.ylan.common.constant;
+
+/**
+ * Redis 缓存常量类
+ *
+ * @author ylan
+ */
+public class RedisCacheConstant {
+
+    /**
+     * 用户注册分布式锁key
+     */
+    public static final String LOCK_USER_REGISTER_KEY = "YlanShortLink:lock_user-register:";
+}
