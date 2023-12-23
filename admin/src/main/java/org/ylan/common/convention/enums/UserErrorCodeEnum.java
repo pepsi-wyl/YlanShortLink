c21119ebb7d0f5ce0a485449fcaf549adfc34ed9
ylan <pepsiwyl@gmail.com>
2023-12-23 21:26:14 +0800

    1.用户错误码

--- admin/src/main/java/org/ylan/common/convention/enums/UserErrorCodeEnum.java
+++ admin/src/main/java/org/ylan/common/convention/enums/UserErrorCodeEnum.java
@@ -20,7 +20,13 @@ public enum UserErrorCodeEnum implements IErrorCode {
 
     USER_SAVE_ERROR("B000203", "用户记录新增失败"),
 
-    USER_UPDATE_ERROR("B000204","用户记录更新失败");
+    USER_UPDATE_ERROR("B000204","用户记录更新失败"),
+
+    USER_LOGIN_ERROR("B000205","用户登录失败,用户名或密码错误"),
+
+    USER_REPEAT_LOGIN_ERROR("B000206","用户重复登录错误"),
+
+    USER_LOGOUT_ERROR("B000207","用户登出失败,用户未登录");
 
     private final String code;
 
