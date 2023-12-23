f92e8ebbfd22a6568e1b956dcaeecadee43de4b7
ylan <pepsiwyl@gmail.com>
2023-12-23 21:25:50 +0800

    1.用户登陆缓存RedisKey前缀

--- admin/src/main/java/org/ylan/common/constant/RedisCacheConstant.java
+++ admin/src/main/java/org/ylan/common/constant/RedisCacheConstant.java
@@ -11,4 +11,9 @@ public class RedisCacheConstant {
      * 用户注册分布式锁key
      */
     public static final String LOCK_USER_REGISTER_KEY = "YlanShortLink:lock_user-register:";
+
+    /**
+     * 用户登陆缓存RedisKey前缀
+     */
+    public static final String LOGIN_PREFIX = "login_";
 }
