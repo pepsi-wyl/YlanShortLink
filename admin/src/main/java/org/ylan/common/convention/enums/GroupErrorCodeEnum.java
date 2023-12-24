9f42fd54889d6ce364e16cc8939c0c8e0983c8b3
ylan <pepsiwyl@gmail.com>
2023-12-24 14:48:11 +0800

    1.分组错误码定义

---
+++ admin/src/main/java/org/ylan/common/convention/enums/GroupErrorCodeEnum.java
@@ -0,0 +1,33 @@
+package org.ylan.common.convention.enums;
+
+import org.ylan.common.convention.errorcode.IErrorCode;
+
+/**
+ * 分组错误码定义定义
+ *
+ * @author ylan
+ */
+
+public enum GroupErrorCodeEnum implements IErrorCode {
+
+    GROUP_SAVE_ERROR("B000301","分组记录新增失败");
+
+    private final String code;
+
+    private final String message;
+
+    GroupErrorCodeEnum(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    @Override
+    public String code() {
+        return code;
+    }
+
+    @Override
+    public String message() {
+        return message;
+    }
+}
