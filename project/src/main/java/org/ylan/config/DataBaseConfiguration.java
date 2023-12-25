3cc02bbf46480a6eb2d67774f88fd51ba19258c1
ylan <pepsiwyl@gmail.com>
2023-12-25 21:18:18 +0800

    1.MP分页插件配置类

---
+++ project/src/main/java/org/ylan/config/DataBaseConfiguration.java
@@ -0,0 +1,27 @@
+package org.ylan.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 数据库持久层配置类
+ *
+ * @author ylan
+ */
+
+@Configuration
+public class DataBaseConfiguration {
+
+    /**
+     * 分页插件
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
