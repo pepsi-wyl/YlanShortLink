2fed666ce2c9a9d45483fd90897bad2650e63ab3
ylan <pepsiwyl@gmail.com>
2023-12-29 09:31:21 +0800

    1.短链接错误码值更新

--- project/src/main/java/org/ylan/common/convention/enums/ShortLinkErrorCodeEnum.java
+++ project/src/main/java/org/ylan/common/convention/enums/ShortLinkErrorCodeEnum.java
@@ -10,11 +10,15 @@ import org.ylan.common.convention.errorcode.IErrorCode;
 
 public enum ShortLinkErrorCodeEnum implements IErrorCode {
 
+    URL_REMOVAL_PROTOCOL_ERROR("A000401","链接删除协议错误"),
+
     SHORT_LINK_GENERATE_ERROR("B000401","短链接频繁生成，请稍后再试"),
 
     SHORT_LINK_SAVE_ERROR("B000402","短链接保存失败"),
 
-    SHORT_LINK_GENERATE_REPEAT_ERROR("B000403","短链接生成重复");
+    SHORT_LINK_GENERATE_REPEAT_ERROR("B000403","短链接生成重复"),
+
+    SHORT_LINK_GOTO_SAVE_ERROR("B000404", "短链跳转接保存失败");
 
     private final String code;
 
