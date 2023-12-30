d7ce6082d8e1a254e898dd718635fe3e5826f6ef
ylan <pepsiwyl@gmail.com>
2023-12-30 20:39:04 +0800

    1.短链接常量类-永久短链接默认缓存有效时间戳

---
+++ project/src/main/java/org/ylan/common/constant/ShortLinkConstant.java
@@ -0,0 +1,16 @@
+package org.ylan.common.constant;
+
+/**
+ * 短链接常量类
+ *
+ * @author ylan
+ */
+
+public class ShortLinkConstant {
+
+    /**
+     * 永久短链接默认缓存有效时间戳，默认一个月
+     */
+    public static final long DEFAULT_CACHE_VALID_TIME = 2626560000L;
+
+}
