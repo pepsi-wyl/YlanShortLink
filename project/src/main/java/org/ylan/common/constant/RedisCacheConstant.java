da3d3202cb5fae6435bc6739fc6feb4afa28cd05
ylan <pepsiwyl@gmail.com>
2023-12-30 20:40:21 +0800

    1.短链接跳转缓存常量和分布式锁常量

--- project/src/main/java/org/ylan/common/constant/RedisCacheConstant.java
+++ project/src/main/java/org/ylan/common/constant/RedisCacheConstant.java
@@ -7,4 +7,18 @@ package org.ylan.common.constant;
  */
 public class RedisCacheConstant {
 
+    /**
+     * 短链接跳转前缀 Key
+     */
+    public static final String GOTO_SHORT_LINK_KEY = "short-link_goto_%s";
+
+    /**
+     * 短链接空值跳转前缀 Key
+     */
+    public static final String GOTO_IS_NULL_SHORT_LINK_KEY = "short-link_is-null_goto_%s";
+
+    /**
+     * 短链接跳转锁前缀 分布式锁 Key
+     */
+    public static final String LOCK_GOTO_SHORT_LINK_KEY = "short-link_lock_goto_%s";
 }
