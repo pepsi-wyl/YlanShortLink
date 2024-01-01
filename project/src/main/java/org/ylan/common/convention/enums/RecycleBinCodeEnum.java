3354e192081676cd1fbef21f213a6c74a8df2f55
ylan <pepsiwyl@gmail.com>
2024-01-01 17:03:08 +0800

    1.回收站错误码-短链接移至回收站失败

---
+++ project/src/main/java/org/ylan/common/convention/enums/RecycleBinCodeEnum.java
@@ -0,0 +1,33 @@
+package org.ylan.common.convention.enums;
+
+import org.ylan.common.convention.errorcode.IErrorCode;
+
+/**
+ * 回收站错误码枚举定义
+ *
+ * @author ylan
+ */
+
+public enum RecycleBinCodeEnum implements IErrorCode {
+
+    RECYCLE_BIN_SAVE_ERROR("B000501", "短链接移至回收站失败");
+
+    private final String code;
+
+    private final String message;
+
+    RecycleBinCodeEnum(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    @Override
+    public String code() {
+        return code;
+    }
+
+    @Override
+    public String message() {
+        return message;
+    }
+}
