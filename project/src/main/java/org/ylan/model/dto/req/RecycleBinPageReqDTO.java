2af220deaba9fd38bc8e629e3f0459c39fcf7983
ylan <pepsiwyl@gmail.com>
2024-01-03 14:48:20 +0800

    1.回收站短链接分页请求-响应参数

---
+++ project/src/main/java/org/ylan/model/dto/req/RecycleBinPageReqDTO.java
@@ -0,0 +1,24 @@
+package org.ylan.model.dto.req;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.ylan.model.entity.ShortLinkDO;
+
+import java.util.List;
+
+/**
+ * 回收站短链接分页请求参数
+ *
+ * @author ylan
+ */
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class RecycleBinPageReqDTO extends Page<ShortLinkDO> {
+
+    /**
+     * 分组标识
+     */
+    private List<String> gidList;
+}
