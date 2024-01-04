d96be7b879d53655a617685ea6f696334806307b
ylan <pepsiwyl@gmail.com>
2024-01-04 11:22:48 +0800

    1.网络工具类开发，并抽取常量

--- common/src/main/java/org/ylan/common/constant/NetConstant.java
+++ common/src/main/java/org/ylan/common/constant/NetConstant.java
@@ -11,16 +11,113 @@ public class NetConstant {
     /**
      * Http协议前缀
      */
-    public final static  String HTTP = "http://";
+    public final static String HTTP = "http://";
 
     /**
      * Https协议前缀
      */
-    public final  static  String HTTPS = "https://";
+    public final static String HTTPS = "https://";
 
     /**
      * URL分隔符
      */
-    public final static  String URL_SPLIT = "/";
+    public final static String URL_SPLIT = "/";
+
+    /**
+     * X-Forwarded-For
+     */
+    public final static String X_FORWARDED_FOR = "X-Forwarded-For";
+
+    /**
+     * Proxy-Client-IP
+     */
+    public final static String PROXY_CLIENT_IP = "Proxy-Client-IP";
+
+    /**
+     * WL-Proxy-Client-IP
+     */
+    public final static String WL_PROXY_CLIENT_IP = "WL-Proxy-Client-IP";
+
+    /**
+     * HTTP_CLIENT_IP
+     */
+    public final static String HTTP_CLIENT_IP = "HTTP_CLIENT_IP";
+
+    /**
+     * HTTP_X_FORWARDED_FOR
+     */
+    public final static String HTTP_X_FORWARDED_FOR = "HTTP_X_FORWARDED_FOR";
+
+    /**
+     * User-Agent
+     */
+    public final static String USR_AGENT = "User-Agent";
+
+    /**
+     * 未知IP
+     */
+    public final static String IP_UNKNOWN = "unknown";
+
+    /**
+     * User-Agent OS标识
+     */
+    public final static String WINDOWS = "windows";
+    public final static String MAC = "mac";
+    public final static String LINUX = "linux";
+    public final static String ANDROID = "android";
+    public final static String IPHONE = "iphone";
+    public final static String IPAD = "ipad";
+
+    /**
+     * OS常量
+     */
+    public final static String WINDOWS_OS = "Windows";
+    public final static String MAC_OS = "Mac OS";
+    public final static String LINUX_OS = "Linux";
+    public final static String ANDROID_OS = "Android";
+    public final static String IPHONE_OS = "IOS";
+    public final static String IPAD_OS = "IPad OS";
+    public final static String UNKNOWN_OS = "Unknown";
+
+    /**
+     * User-Agent 浏览器标识
+     */
+    public final static String EDGE = "edg";
+    public final static String CHROME = "chrome";
+    public final static String SAFARI = "safari";
+    public final static String FIREFOX = "firefox";
+    public final static String OPERA = "opera";
+    public final static String IE = "msie";
+    public final static String IE11 = "trident";
+
+    /**
+     * 浏览器常量
+     */
+    public final static String EDGE_BROWSER = "Microsoft Edge";
+    public final static String CHROME_BROWSER = "Google Chrome";
+    public final static String SAFARI_BROWSER = "Apple Safari";
+    public final static String FIREFOX_BROWSER = "Mozilla Firefox";
+    public final static String OPERA_BROWSER = "Opera";
+    public final static String IE_BROWSER = "Internet Explorer";
+    public final static String UNKNOWN_BROWSER = "Unknown";
+
+    /**
+     * User-Agent 设备标识
+     */
+    public final static String PC = "pc";
+    public final static String MOBILE = "mobile";
+
+    /**
+     * 设备常量
+     */
+    public final static String PC_DEVICE = "PC";
+    public final static String MOBILE_DEVICE = "Mobile";
+
+    /**
+     * WIFI 网段
+     */
+    public final static String WIFI_IP_1 = "192.168.";
+    public final static String WIFI_IP_2 = "10";
+    public final static String WIFI_IP_3 = "172.";
 
 }
--- common/src/main/java/org/ylan/utils/LinkUtil.java
+++ common/src/main/java/org/ylan/utils/LinkUtil.java
@@ -2,10 +2,12 @@ package org.ylan.utils;
 
 import cn.hutool.core.date.DateUnit;
 import cn.hutool.core.date.DateUtil;
+import jakarta.servlet.http.HttpServletRequest;
 
 import java.util.Date;
 import java.util.Optional;
 
+import static org.ylan.common.constant.NetConstant.*;
 import static org.ylan.common.constant.ShortLinkConstant.DEFAULT_CACHE_VALID_TIME;
 
 /**
@@ -26,6 +28,111 @@ public class LinkUtil {
         return Optional.ofNullable(validDate)
                 .map(each -> DateUtil.between(new Date(), each, DateUnit.MS))
                 .orElse(DEFAULT_CACHE_VALID_TIME);
+    }
+
+    /**
+     * 获取用户访问真实IP
+     *
+     * @param request HttpServletRequest请求
+     * @return 用户访问真实IP
+     */
+    public static String getActualIp(HttpServletRequest request) {
+        String ipAddress = request.getHeader(X_FORWARDED_FOR);
+        if (ipAddress == null || ipAddress.isEmpty() || IP_UNKNOWN.equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getHeader(PROXY_CLIENT_IP);
+        }
+        if (ipAddress == null || ipAddress.isEmpty() || IP_UNKNOWN.equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getHeader(WL_PROXY_CLIENT_IP);
+        }
+        if (ipAddress == null || ipAddress.isEmpty() || IP_UNKNOWN.equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getHeader(HTTP_CLIENT_IP);
+        }
+        if (ipAddress == null || ipAddress.isEmpty() || IP_UNKNOWN.equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getHeader(HTTP_X_FORWARDED_FOR);
+        }
+        if (ipAddress == null || ipAddress.isEmpty() || IP_UNKNOWN.equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getRemoteAddr();
+        }
+        return ipAddress;
+    }
+
+    /**
+     * 获取用户访问操作系统
+     *
+     * @param request HttpServletRequest请求
+     * @return 用户访问操作系统
+     */
+    public static String getOs(HttpServletRequest request) {
+        String userAgent = request.getHeader(USR_AGENT);
+        if (userAgent.toLowerCase().contains(WINDOWS)) {
+            return WINDOWS_OS;
+        } else if (userAgent.toLowerCase().contains(MAC)) {
+            return MAC_OS;
+        } else if (userAgent.toLowerCase().contains(LINUX)) {
+            return LINUX_OS;
+        } else if (userAgent.toLowerCase().contains(ANDROID)) {
+            return ANDROID_OS;
+        } else if (userAgent.toLowerCase().contains(IPHONE)) {
+            return IPHONE_OS;
+        } else if (userAgent.toLowerCase().contains(IPAD)) {
+            return IPAD_OS;
+        } else {
+            return UNKNOWN_OS;
+        }
+    }
+
+    /**
+     * 获取用户访问浏览器
+     *
+     * @param request HttpServletRequest请求
+     * @return 用户访问浏览器
+     */
+    public static String getBrowser(HttpServletRequest request) {
+        String userAgent = request.getHeader(USR_AGENT);
+        if (userAgent.toLowerCase().contains(EDGE)) {
+            return EDGE_BROWSER;
+        } else if (userAgent.toLowerCase().contains(CHROME)) {
+            return CHROME_BROWSER;
+        } else if (userAgent.toLowerCase().contains(SAFARI)) {
+            return SAFARI_BROWSER;
+        } else if (userAgent.toLowerCase().contains(FIREFOX)) {
+            return FIREFOX_BROWSER;
+        } else if (userAgent.toLowerCase().contains(OPERA)) {
+            return OPERA_BROWSER;
+        } else if (userAgent.toLowerCase().contains(IE) || userAgent.toLowerCase().contains(IE11)) {
+            return IE_BROWSER;
+        } else {
+            return UNKNOWN_BROWSER;
+        }
+    }
+
+    /**
+     * 获取用户访问设备
+     *
+     * @param request HttpServletRequest请求
+     * @return 用户访问设备
+     */
+    public static String getDevice(HttpServletRequest request) {
+        String userAgent = request.getHeader(USR_AGENT);
+        if (userAgent.toLowerCase().contains(MOBILE)) {
+            return MOBILE_DEVICE;
+        }
+        return PC_DEVICE;
+    }
+
+    /**
+     * 获取用户访问网络
+     *
+     * @param request HttpServletRequest请求
+     * @return 用户访问网络
+     */
+    public static String getNetwork(HttpServletRequest request) {
+        String actualIp = getActualIp(request);
+        return
+                actualIp.startsWith(WIFI_IP_1)
+                        || actualIp.startsWith(WIFI_IP_2)
+                        || actualIp.startsWith(WIFI_IP_3)
+                        ? "WIFI" : "Mobile";
     }
 
 }
