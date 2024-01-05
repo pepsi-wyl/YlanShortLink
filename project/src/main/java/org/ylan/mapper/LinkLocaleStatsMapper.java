b2b9fcb3d2cb30fe665a36b715db31d924053d8c
ylan <pepsiwyl@gmail.com>
2024-01-05 10:57:36 +0800

    1.短链接地区统计访问持久层

---
+++ project/src/main/java/org/ylan/mapper/LinkLocaleStatsMapper.java
@@ -0,0 +1,21 @@
+package org.ylan.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Param;
+import org.ylan.model.entity.LinkLocaleStatsDO;
+
+/**
+ * 短链接地区统计访问持久层
+ *
+ * @author ylan
+ */
+
+public interface LinkLocaleStatsMapper extends BaseMapper<LinkLocaleStatsMapper> {
+
+    /**
+     * 短链接地区统计访问监控
+     *
+     * @param linkLocaleStats 短链接地区统计访问实体类
+     */
+    void shortLinkLocaleState(@Param("linkLocaleStats") LinkLocaleStatsDO linkLocaleStats);
+}
