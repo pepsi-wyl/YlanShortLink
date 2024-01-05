2e1587c987b0f1dd51a1a54f790dddd8d418a639
ylan <pepsiwyl@gmail.com>
2024-01-05 10:58:46 +0800

    1.API常量-高德地图

---
+++ common/src/main/java/org/ylan/common/constant/ApiConstant.java
@@ -0,0 +1,61 @@
+package org.ylan.common.constant;
+
+/**
+ * API常量
+ *
+ * @author ylan
+ */
+
+public class ApiConstant {
+
+    /**
+     *  高德地图API 参数 Key
+     */
+    public static final String AMAP_KEY = "key";
+
+    /**
+     *  高德地图API 参数 ip
+     */
+    public static final String AMAP_IP = "ip";
+
+    /**
+     * 高德地图API 返回参数 infocode
+     */
+    public static final String AMAP_RESP_INFOCODE = "infocode";
+
+    /**
+     * 高德地图API 返回参数 infocode成功值
+     */
+    public static final String AMAP_RESP_INFOCODE_SUCCESS= "10000";
+
+    /**
+     * 高德地图API 返回参数 province
+     */
+    public static final String AMAP_RESP_PROVINCE = "province";
+
+    /**
+     * 高德地图API 返回参数 province空值
+     */
+    public static final String AMAP_RESP_PROVINCE_EMPTY = "[]";
+
+    /**
+     * 高德地图API 返回参数 city
+     */
+    public static final String AMAP_RESP_CITY = "city";
+
+    /**
+     * 高德地图API 返回参数 city空值
+     */
+    public static final String AMAP_RESP_CITY_EMPTY = "[]";
+
+    /**
+     * 高德地图API 返回参数 adcode
+     */
+    public static final String AMAP_RESP_ADCODE= "adcode";
+
+    /**
+     * 高德地图API 返回参数 adcode空值
+     */
+    public static final String AMAP_RESP_ADCODE_EMPTY = "[]";
+
+}
