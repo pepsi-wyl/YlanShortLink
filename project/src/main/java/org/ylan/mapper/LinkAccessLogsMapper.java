aec0cb6d8ad72cf7df5c6aa1438ad2cbbdcded11
ylan <pepsiwyl@gmail.com>
2024-01-07 20:37:09 +0800

    1.短链接访问日志监控持久层

---
+++ project/src/main/java/org/ylan/mapper/LinkAccessLogsMapper.java
@@ -0,0 +1,16 @@
+package org.ylan.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.ylan.model.entity.LinkAccessLogsDO;
+
+/**
+ * 短链接访问日志监控持久层
+ *
+ * @author ylan
+ */
+
+
+public interface LinkAccessLogsMapper extends BaseMapper<LinkAccessLogsDO> {
+
+
+}
