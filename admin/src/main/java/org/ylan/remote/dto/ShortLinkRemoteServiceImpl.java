c5bb75d2d94d06e1210a92f048eac447207ff0ca
ylan <pepsiwyl@gmail.com>
2024-01-08 16:16:33 +0800

    1.远程调用MDC使用，之后重构为SpringCloud调用

--- admin/src/main/java/org/ylan/remote/dto/ShortLinkRemoteServiceImpl.java
+++ admin/src/main/java/org/ylan/remote/dto/ShortLinkRemoteServiceImpl.java
@@ -1,11 +1,13 @@
 package org.ylan.remote.dto;
 
+import cn.hutool.http.HttpRequest;
 import cn.hutool.http.HttpUtil;
 import com.alibaba.fastjson2.JSON;
 import com.alibaba.fastjson2.TypeReference;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
 import org.springframework.stereotype.Service;
 import org.ylan.common.convention.result.Result;
 import org.ylan.remote.dto.req.*;
@@ -18,6 +20,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static org.ylan.common.constant.LogsConstant.TRACE_ID;
+
 /**
  * 短链接中台服务
  *
@@ -29,7 +33,7 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class ShortLinkRemoteServiceImpl implements ShortLinkRemoteService{
 
-    // todo 后续重构为 SpringCloud Feign 调用
+    // todo 后续重构为 SpringCloud Feign 调用 并加入TraceID
     private static final String HOST = "http://127.0.0.1:8001";
 
     @Override
@@ -38,7 +42,8 @@ public class ShortLinkRemoteServiceImpl implements ShortLinkRemoteService{
         String jsonString = JSON.toJSONString(requestParam);
         log.info("【 创建短链接-POST-请求参数 】【 {} 】", jsonString);
         // 发送请求
-        String resultBodyStr = HttpUtil.post(HOST + "/api/short-link/v1/create", jsonString);
+        String resultBodyStr = HttpRequest.post(HOST + "/api/short-link/v1/create").header(TRACE_ID, MDC.get(TRACE_ID)).body(jsonString).execute().body();
+//        String resultBodyStr = HttpUtil.post(HOST + "/api/short-link/v1/create", jsonString);
         log.info("【 创建短链接响应结果 】 【 {} 】", resultBodyStr);
         // 转化响应
         return JSON.parseObject(resultBodyStr, new TypeReference<>() {});
