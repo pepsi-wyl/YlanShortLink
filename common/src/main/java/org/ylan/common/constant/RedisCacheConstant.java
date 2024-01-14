432b7ba884ae9fcd9454ff76cfba4b93003fa76e
ylan <pepsiwyl@gmail.com>
2024-01-14 22:05:15 +0800

    1.Redis 缓存常量类 ：短链接修改分组 ID 锁前缀 Key，短链接延迟队列消费统计 Key

--- common/src/main/java/org/ylan/common/constant/RedisCacheConstant.java
+++ common/src/main/java/org/ylan/common/constant/RedisCacheConstant.java
@@ -51,4 +51,14 @@ public class RedisCacheConstant {
      * 短链接监控状态UIP状态前缀
      */
     public static final String SHORT_LINK_STATS_UIP_PREFIX = "short-link:stats:uip:";
+
+    /**
+     * 短链接修改分组 ID 锁前缀 Key
+     */
+    public static final String LOCK_GID_UPDATE_KEY = "short-link_lock_update-gid_%s";
+
+    /**
+     * 短链接延迟队列消费统计 Key
+     */
+    public static final String DELAY_QUEUE_STATS_KEY = "short-link_delay-queue:stats";
 }
