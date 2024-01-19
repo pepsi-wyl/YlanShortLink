55934fbb1c98066775f2d78464a2fb6eea8ca534
ylan <pepsiwyl@gmail.com>
2024-01-19 21:40:56 +0800

    1.openFeign配置输出日志-并配置服务调用TRACE_ID

--- admin/src/main/java/org/ylan/config/OpenFeignConfig.java
+++ admin/src/main/java/org/ylan/config/OpenFeignConfig.java
@@ -1,7 +1,14 @@
 package org.ylan.config;
 
+import feign.Logger;
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import org.slf4j.MDC;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import static org.ylan.common.constant.LogsConstant.TRACE_ID;
 
 /**
  * OpenFeign配置类
@@ -11,6 +18,16 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableFeignClients("org.ylan.remote")
-public class OpenFeignConfig {
+public class OpenFeignConfig implements RequestInterceptor {
 
+    @Override
+    public void apply(RequestTemplate requestTemplate) {
+        // 设置traceId到header中
+        requestTemplate.header(TRACE_ID, MDC.get(TRACE_ID));
+    }
+
+    @Bean
+    public Logger.Level feignLoggerLevel(){
+        return Logger.Level.FULL;
+    }
 }
