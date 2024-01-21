3853e744eb25e655ee3b34d914c3ad33195493b3
ylan <pepsiwyl@gmail.com>
2024-01-21 21:54:13 +0800

    1.gateway初始化

---
+++ gateway/src/main/java/org/ylan/YlanShortLinkGatewayApplication.java
@@ -0,0 +1,17 @@
+package org.ylan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Gateway启动类
+ *
+ * @author ylan
+ */
+
+@SpringBootApplication
+public class YlanShortLinkGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(YlanShortLinkGatewayApplication.class, args);
+    }
+}
