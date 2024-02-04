1f4746f5cd1efc254810bd9329660bdfe3af7b6a
ylan <pepsiwyl@gmail.com>
2024-02-04 15:36:21 +0800

    1.Gateway过滤器顺序常量

---
+++ common/src/main/java/org/ylan/common/constant/GateWayFilerOrderConstant.java
@@ -0,0 +1,21 @@
+package org.ylan.common.constant;
+
+/**
+ * Gateway过滤器顺序常量
+ *
+ * @author ylan
+ */
+
+public class GateWayFilerOrderConstant {
+
+    /**
+     * 链路追踪过滤器
+     */
+    public static final int TRACE_INFO_FILTER_ORDER = -100;
+
+    /**
+     * Gateway日志过滤器
+     */
+    public static final int GATEWAY_LOG_FILTER_ORDER = -90;
+
+}
