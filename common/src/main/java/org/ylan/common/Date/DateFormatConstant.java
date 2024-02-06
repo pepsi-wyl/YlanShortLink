ad02f987441bdbac96ba71002fc2f1c4648b0fa8
ylan <pepsiwyl@gmail.com>
2024-02-06 10:14:10 +0800

    1.时间格式化常量

---
+++ common/src/main/java/org/ylan/common/Date/DateFormatConstant.java
@@ -0,0 +1,18 @@
+package org.ylan.common.Date;
+
+import java.text.SimpleDateFormat;
+
+/**
+ * 时间格式化常量
+ *
+ * @author ylan
+ */
+
+public class DateFormatConstant {
+
+    /**
+     * 日期格式化 HH:mm:ss
+     */
+    public static final SimpleDateFormat DATE_FORMAT_HH_MM_SS = new SimpleDateFormat("HH:mm:ss");
+
+}
