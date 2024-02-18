dc4dcf91d3fb4940ff61932d409e00f848fe4b01
ylan <pepsiwyl@gmail.com>
2024-02-18 20:41:08 +0800

    1. Task模块 SpringBoot启动类

---
+++ task/src/main/java/org/ylan/YlanShortLinkTaskApplication.java
@@ -0,0 +1,30 @@
+package org.ylan;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Date;
+
+import static org.ylan.common.Date.DateFormatConstant.DATE_FORMAT_HH_MM_SS;
+
+/**
+ * SpringBoot-Task启动类
+ *
+ * @author ylan
+ */
+
+@SpringBootApplication
+public class YlanShortLinkTaskApplication {
+
+    /**
+     * 日志记录器
+     */
+    private static final Logger log = LoggerFactory.getLogger(YlanShortLinkTaskApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(YlanShortLinkTaskApplication.class, args);
+        log.info("YlanShortLinkTaskApplication started successfully, Current Thread : {}, The time is now {}", Thread.currentThread().getName(), DATE_FORMAT_HH_MM_SS.format(new Date()));
+    }
+}
