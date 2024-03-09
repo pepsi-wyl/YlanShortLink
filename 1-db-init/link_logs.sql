28c66505f2d1a9b1c742e2a28a5df8d663e71403
ylan <pepsiwyl@gmail.com>
2024-03-09 21:43:09 +0800

    1.网关历史日志表

--- 1-db-init/link_logs.sql
+++ 1-db-init/link_logs.sql
@@ -24,3 +24,24 @@ CREATE TABLE `t_link_gateway_log`
     primary key (`trace_id`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
+
+# 网关日志表备份历史表-记录30天之前的日志记录
+CREATE TABLE `t_link_gateway_log_history`
+(
+    `trace_id`              varchar(48)  COMMENT '链路追踪ID',
+    `ip`                    varchar(48)  DEFAULT NULL COMMENT 'ip',
+    `schema`                varchar(8)   DEFAULT NULL COMMENT '协议',
+    `request_method`        varchar(8)   DEFAULT NULL COMMENT '请求方法',
+    `request_path`          varchar(128) DEFAULT NULL COMMENT '请求路径',
+    `request_content_type`  varchar(128) DEFAULT NULL COMMENT '请求数据类型',
+    `request_body`          TEXT         DEFAULT NULL COMMENT '请求体',
+    `response_content_type` varchar(128) DEFAULT NULL COMMENT '返回数据类型',
+    `response_data`         TEXT         DEFAULT NULL COMMENT '响应体',
+    `request_time`          datetime     DEFAULT NULL COMMENT '请求时间',
+    `response_time`         datetime     DEFAULT NULL COMMENT '响应时间',
+    `execute_time`          bigint(20)   DEFAULT NULL COMMENT '执行时间',
+    `target_server`         varchar(128) DEFAULT NULL COMMENT '访问实例',
+    `route_config`          TEXT         DEFAULT NULL COMMENT '路由配置',
+    primary key (`trace_id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
