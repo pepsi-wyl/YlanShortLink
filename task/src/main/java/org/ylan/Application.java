bf52f555a848f777af8cd1ae650f28fa1f875c6b
ylan <pepsiwyl@gmail.com>
2024-03-09 22:45:02 +0800

    1.伪Quartz集群启动类

---
+++ task/src/main/java/org/ylan/Application.java
@@ -0,0 +1,16 @@
+//package org.ylan;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+///**
+// * @author by ylan
+// */
+//
+//@SpringBootApplication
+//public class Application {
+//    public static void main(String[] args) {
+//        System.setProperty("server.port", "0");
+//        SpringApplication.run(Application.class, args);
+//    }
+//}
